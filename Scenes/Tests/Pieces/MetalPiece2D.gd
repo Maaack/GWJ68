7f6535589cda016658257e3a45a2eb8753d816a5
Maaack <marek.belski@gmail.com>
2024-04-14 21:52:45 -0700

    Includes rotations in forging pieces together.

--- Scenes/Tests/Pieces/MetalPiece2D.gd
+++ Scenes/Tests/Pieces/MetalPiece2D.gd
@@ -6,9 +6,14 @@ signal clicked
 var held = false
 
 func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed:
-			clicked.emit(self)
+	if event.is_action_pressed("select"):
+		clicked.emit(self)
+	if event.is_action_pressed("rotate_left"):
+		if held:
+			rotation += PI/4
+	if event.is_action_pressed("rotate_right"):
+		if held:
+			rotation -= PI/4
 
 func _physics_process(delta):
 	if held:
@@ -39,8 +44,9 @@ func _on_body_entered(body):
 	if body is MetalPiece2D:
 		if body.held:
 			var position_offset = position - body.position
+			var _transform = transform.rotated(rotation - body.rotation)
 			var _transformed_polygon: PackedVector2Array = []
-			for _vector in get_polygon(): _transformed_polygon.append(transform.basis_xform(_vector + position_offset))
+			for _vector in get_polygon(): _transformed_polygon.append(_transform.basis_xform(_vector + position_offset))
 			var new_polygons = Geometry2D.merge_polygons(_transformed_polygon, body.get_polygon())
 			body.call_deferred("set_polygon", new_polygons[0])
 			queue_free()
