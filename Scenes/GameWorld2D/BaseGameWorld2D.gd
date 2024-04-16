a1bd9a724880cc2e33435706215693c1bae7469c
Maaack <marek.belski@gmail.com>
2024-04-15 19:08:51 -0700

    Starts base game world within game UI.

---
+++ Scenes/GameWorld2D/BaseGameWorld2D.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@export var starting_money : int = 0
+
+@onready var total_money : int = starting_money
+
+func _unhandled_input(event):
+	if event.is_action_pressed("interact"):
+		$SpawnController.spawn()
+
+
+func _on_trade_controller_piece_sold(value):
+	total_money += value
