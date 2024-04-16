3516c2f7f231bf79506938ccaa98c5dc09519e51
Maaack <marek.belski@gmail.com>
2024-04-16 14:56:37 -0700

    Makes main polygon from one shape.

--- Scenes/Pieces/MetalPiece2D.gd
+++ Scenes/Pieces/MetalPiece2D.gd
@@ -6,6 +6,7 @@ signal clicked
 
 var held : bool = false
 @onready var heat_controller : HeatController = $HeatController
+@onready var polygon_2d : Polygon2D = $Polygon2D
 
 func _ready():
 	freeze_mode = RigidBody2D.FREEZE_MODE_KINEMATIC
@@ -24,15 +25,21 @@ func _physics_process(delta):
 	if held:
 		global_transform.origin = get_global_mouse_position()
 
-func merge_to(other_piece : MetalPiece2D):
+func merge_to(relative_polygon : PackedVector2Array, other_piece : MetalPiece2D):
 	for child in get_children():
-		if child is CollisionPolygon2D or child is Polygon2D:
-			child.call_deferred("reparent", other_piece)
+		if child is CollisionPolygon2D:
+			child.reparent(other_piece)
 	other_piece.mass += mass
+	other_piece.add_polygon(relative_polygon)
 	queue_free()
 
 func get_polygon() -> PackedVector2Array:
-	return $Polygon2D.polygon
+	return polygon_2d.polygon
+
+func add_polygon(polygon : PackedVector2Array):
+	var new_polygons = Geometry2D.merge_polygons(get_polygon(), polygon)
+	if new_polygons.size() == 1:
+		polygon_2d.polygon = new_polygons[0]
 
 func get_surface_area() -> float:
 	var polygon := get_polygon()
@@ -65,14 +72,33 @@ func move_origin(new_origin_offset : Vector2):
 			continue
 		child.position += new_origin_offset
 
-func _can_bind_with_piece(piece : MetalPiece2D) -> bool:
-	var _this_temp = heat_controller.body_temperature
-	var _other_temp = piece.heat_controller.body_temperature
-	var _min_melting_point = min(heat_controller.melting_point, piece.heat_controller.melting_point)
-	return (_this_temp + _other_temp) / 2 > _min_melting_point
-		
+func _get_polygon_relative_to_piece(other_piece : MetalPiece2D) -> PackedVector2Array:
+	var position_offset = polygon_2d.global_position - other_piece.polygon_2d.global_position
+	var _transform = transform.rotated(other_piece.global_rotation)
+	var _transformed_polygon: PackedVector2Array = []
+	var _transformed_offset = position_offset.rotated(-other_piece.global_rotation)
+	for _vector in get_polygon(): _transformed_polygon.append(_transform.basis_xform(_vector) + _transformed_offset)
+	return _transformed_polygon
+
+func _polygons_overlap(polygon_a : PackedVector2Array, polygon_b : PackedVector2Array) -> bool:
+	var new_polygons = Geometry2D.merge_polygons(polygon_a, polygon_b)
+	if new_polygons.size() == 1:
+		return true
+	return false
+
+func _is_temp_greater_than_melting(heat_controller_a : HeatController, heat_controller_b : HeatController):
+	var _temp_a = heat_controller_a.body_temperature
+	var _temp_b = heat_controller_b.body_temperature
+	var _min_melting_point = min(heat_controller_a.melting_point, heat_controller_b.melting_point)
+	return (_temp_a + _temp_b) / 2 > _min_melting_point
+
+func _can_merge_with_piece(polygon : PackedVector2Array, piece : MetalPiece2D) -> bool:
+	var _melting_temp = _is_temp_greater_than_melting(heat_controller, piece.heat_controller)
+	var _polygons_overlap = _polygons_overlap(polygon, piece.get_polygon())
+	return _melting_temp and _polygons_overlap
 
 func _on_body_entered(body):
-	if body is MetalPiece2D:
-		if _can_bind_with_piece(body):
-			merge_to(body)
+	if body is MetalPiece2D and not body.held:
+		var relative_polygon = _get_polygon_relative_to_piece(body)
+		if _can_merge_with_piece(relative_polygon, body):
+			call_deferred("merge_to", relative_polygon, body)
