f0995f107e5aa9a8f921a003b01520f4796027ae
Maaack <marek.belski@gmail.com>
2024-04-17 13:59:03 -0700

    Adds a day timer progress bar.

--- Scenes/GameUI/GameUI.gd
+++ Scenes/GameUI/GameUI.gd
@@ -3,6 +3,18 @@ extends Control
 const MONEY_LABEL_TEXT = "$ %d"
 
 @onready var money_label : Label = %MoneyLabel
+@onready var night_time_control : Control = %NightTimeControl
+@onready var game_world : Node = $SubViewportContainer/SubViewport/FirstGameWorld2D
 
 func _on_first_game_world_2d_money_updated(new_value):
 	money_label.text = MONEY_LABEL_TEXT % new_value
+
+func _on_first_game_world_2d_day_progress_updated(new_value):
+	%DayProgressBar.value = new_value
+
+func _on_next_day_button_pressed():
+	game_world.start_day()
+	night_time_control.hide()
+
+func _on_first_game_world_2d_day_ended():
+	night_time_control.show()
--- Scenes/GameWorld2D/BaseGameWorld2D.gd
+++ Scenes/GameWorld2D/BaseGameWorld2D.gd
@@ -1,8 +1,23 @@
 extends Node2D
 
 signal money_updated(new_value : int)
+signal day_progress_updated(new_value : float)
+signal day_count_updated(new_value : int)
+signal day_ended()
 
 @export var starting_money : int = 0
+@export var day_length : float = 60.0
+
+var day_progress : float = 0.0 :
+	set(value):
+		day_progress = value
+		day_progress_updated.emit(day_progress)
+
+var day_count : int = 0 : 
+	set(value):
+		day_count = value
+		day_count_updated.emit(day_count)
+		
 
 @onready var _money : int = starting_money :
 	set(value):
@@ -13,5 +28,23 @@ func _unhandled_input(event):
 	if event.is_action_pressed("interact"):
 		$SpawnController.spawn()
 
+
 func _on_trade_controller_piece_sold(value):
 	_money += value
+
+func start_day():
+	day_count += 1
+	$DayTimer.start(day_length)
+
+func _end_day():
+	day_ended.emit()
+
+func _process(delta):
+	var time_passed : float = day_length - $DayTimer.time_left
+	day_progress = time_passed / day_length
+
+func _ready():
+	start_day()
+
+func _on_day_timer_timeout():
+	_end_day()
